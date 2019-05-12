3ceb05d23ba7caf11e2f4a86303540d9f2e43c62
Alexander Gschwend <bralgs@hotmail.com>
2019-05-12 19:37:17 +0200

    ASP.NET MVC 5 Course - Section 11 - Deployment and Final Touch

--- Vidly/Vidly/App_Start/Startup.Auth.cs
+++ Vidly/Vidly/App_Start/Startup.Auth.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
@@ -55,8 +56,8 @@ namespace Vidly
             //   consumerSecret: "");
 
             app.UseFacebookAuthentication(
-               appId: "867263356944893",
-               appSecret: "5cf324397ec097caa5b9eed621cef9ff");
+               appId: ConfigurationManager.AppSettings["FacebookAppId"],
+               appSecret: ConfigurationManager.AppSettings["FacebookAppSecret"]);
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
